27292ff2fb18cc6aa72a7220ce75bf8835d8532d
Dennis Kinanga <denniskinanga6@gmail.com>
2024-07-12 02:20:14 +0300

    listview

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    final List<String> friends = ["Denny", "Kinanga", "Shuvo", "Rodoshi"];
+    
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My App"),
+        ),
+        body:ListView.builder(
+          itemCount: friends.length,
+          itemBuilder: (context,index){
+          return Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.amber,
+            margin: const EdgeInsets.only(bottom: 1),
+            child: Text(friends[index]));
+        }),
+      ),
+    );
+  }
+}
