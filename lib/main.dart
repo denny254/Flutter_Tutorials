72ee0155e29b92ee34e632cb97515aef8a68278a
Dennis Kinanga <denniskinanga6@gmail.com>
2024-07-13 02:02:46 +0300

    naviagte

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,53 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    
-    
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter APP',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("My App"),
+          title: const Text("Tutor"),
         ),
-        body: const MyFirstStatfulWidget()),
+        body: const RouteOne()),
     );
   }
 }
   
 
+class RouteOne extends StatelessWidget {
+  const RouteOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: ElevatedButton(onPressed: () => {
+       Navigator.push(
+        context, 
+        MaterialPageRoute(builder: (context) => const RouteTwo() ,))
+      }, child: const Text("Navigate")),
+    );
+  }
+}
+
+
+class RouteTwo extends StatelessWidget {
+  const RouteTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Route two"),
+      ),
+      body: ElevatedButton(onPressed: () => {
+        Navigator.pop(context)
+      },
+      child: const Text("Button 2"),),
+    );
+  }
+}
 
 class MyFirstStatfulWidget extends StatefulWidget {
   const MyFirstStatfulWidget({super.key});
