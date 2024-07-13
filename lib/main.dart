8958657d1b3be76c4d42e8b9cec0c9d3ac5ec170
Dennis Kinanga <denniskinanga6@gmail.com>
2024-07-13 03:53:14 +0300

    simple-ui

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,51 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Tutor"),
         ),
-        body: const RouteOne()),
+        body: const StarWars()),
     );
   }
 }
   
+
+class StarWars extends StatelessWidget {
+  const StarWars({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Row(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("assets/images/finalme.png"),
+              ),
+              SizedBox(width: 20,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Guru Denny", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                  Text("Angel of Flutter"),
+                ],
+              )
+            ],
+          ),
+          const Divider(
+            color: Colors.grey,
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: const Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
 class RouteOne extends StatelessWidget {
   const RouteOne({super.key});
